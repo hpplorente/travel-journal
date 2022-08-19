6dc46c57b6aeb568ee3fc403d5a7841a6eb2fca8
Henri Paul <kiddolorente@gmail.com>
2022-08-18 23:31:32 -0400

    added some styling

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,24 @@
 import React from "react";
+import "./card.css";
 
 function Card(props) {
   console.log(props.item.description);
   console.log(props.item.startDate);
   return (
     <div className="container">
-      <div className="card">
-        <img src={props.item.image} />
+      <img src={props.item.image} />
+      <div>
         <div>
-          <div>
-            <span>{props.item.location}</span>
-            <span>
-              <a href={props.item.googleMaps}>View on Google Maps</a>
-            </span>
-          </div>
-          <h3>{props.item.title}</h3>
-          <p>
-            {props.item.startDate} - {props.item.endDate}
-          </p>
-          <p>{props.item.description}</p>
+          <span>{props.item.location}</span>
+          <span>
+            <a href={props.item.googleMaps}>View on Google Maps</a>
+          </span>
         </div>
+        <h3>{props.item.title}</h3>
+        <p>
+          {props.item.startDate} - {props.item.endDate}
+        </p>
+        <p>{props.item.description}</p>
       </div>
     </div>
   );
